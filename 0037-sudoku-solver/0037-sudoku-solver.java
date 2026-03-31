404ed1e6e6e9b162f42b33dfee8ab94bb364d268
YadnikBangale <yadnik.bangale@gmail.com>
2026-03-31 23:44:28 +0530

    Time: 222 ms (59.65%), Space: 42.4 MB (98.62%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,56 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+
+                if (board[row][col] == '.') {
+
+                    for (char num = '1'; num <= '9'; num++) {
+
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+
+                            if (solve(board)) return true;
+
+                            // backtrack
+                            board[row][col] = '.';
+                        }
+                    }
+
+                    return false; // no valid number found
+                }
+            }
+        }
+        return true; // solved
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+
+        // Check row
+        for (int j = 0; j < 9; j++) {
+            if (board[row][j] == num) return false;
+        }
+
+        // Check column
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) return false;
+        }
+
+        // Check 3x3 box
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[startRow + i][startCol + j] == num)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
