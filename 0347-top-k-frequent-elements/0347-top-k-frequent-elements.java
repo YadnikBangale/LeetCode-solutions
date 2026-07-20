27b70ddece52a5fc20c29ef2d096908d39013290
YadnikBangale <yadnik.bangale@gmail.com>
2026-07-21 02:18:50 +0530

    Time: 17 ms (19.67%), Space: 47.4 MB (91.7%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,54 +1,28 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-
-        if(nums.length == 0) {
-            return new int[0];
-        }
-        int max = nums[0];
-        int min = nums[0];
-
-        for(int i = 0 ; i < nums.length ; i++) {
-            if(nums[i] > max) {
-                max = nums[i];
-            }
-            if(nums[i] < min) {
-                min = nums[i];
-            }
-        }
-        int shift = -min;
-        int arr[] = new int[max - min + 1];
-
-        for(int i = 0 ; i < nums.length ; i++) {
-            arr[nums[i] + shift]++;
-        }
-
-        int result[] = new int[k];
-        int index = 0;
         
-        while(index < k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-            int maxfreq = -1;
-            int num = -1;
+        for(int i = 0 ; i < nums.length ; i++) {
 
-            for(int i = 0 ; i < arr.length ; i++) {
-
-                if(arr[i] > maxfreq) {
-                    maxfreq = arr[i];
-                    num = i;
-                }
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i], 1);
             }
 
-            if(num == -1) {
-                break;
+            else {
+                map.put(nums[i], map.get(nums[i])+1);
             }
-
-            result[index] = num - shift;
-            index++;
-
-            arr[num] = -1;
         }
 
-        return result;
+        List<Map.Entry<Integer, Integer>>list = new ArrayList<>(map.entrySet());
+        Collections.sort(list, (a, b) -> b.getValue() - a.getValue());
 
+        int ans[] = new int[k];
+
+        for(int i = 0 ; i < k ; i++) {
+            ans[i] = list.get(i).getKey();
+        }
+
+        return ans;
     }
 }
